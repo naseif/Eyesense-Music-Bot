d16d5b200467e2518c3c35e0e15167c04f45c6d0
naseif <d2mds21@gmail.com>
2021-08-26 14:38:28 +0200

    [New] Added get user Avatar command

---
+++ commands/misc/avatar.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("useravatar")
+    .setDescription("Sends the user's avatar")
+    .addUserOption((option) =>
+      option.setName("user").setDescription("Select a user").setRequired(true)
+    ),
+  async execute(interaction, client) {
+    const user = interaction.options.getUser("user");
+    await interaction.deferReply();
+    const embed = {
+      color: "#9dcc37",
+      fields: [
+        {
+          name: "User Avatar For: ",
+          value: `${user}`,
+          inline: true,
+        },
+      ],
+      image: {
+        url: `${user.avatarURL({
+          format: "png",
+          dynamic: true,
+          size: 1024,
+        })}`,
+      },
+      timestamp: new Date(),
+    };
+
+    await interaction.followUp({ embeds: [embed] });
+  },
+};
