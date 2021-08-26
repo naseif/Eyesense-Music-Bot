e303924d9a4baf130bc41284bdda40f46498e45c
naseif <d2mds21@gmail.com>
2021-08-26 12:43:52 +0200

    [New] Added new funny commands

---
+++ commands/fun/insult.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { embedMessage } = require("../../modules/embedSimple");
+const fetch = require("node-fetch");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("insult")
+    .setDescription("insult your fav user <3")
+    .addUserOption((option) =>
+      option.setName("user").setDescription("Select a user")
+    ),
+  async execute(interaction, client) {
+    const user = interaction.options.getUser("user");
+    await interaction.deferReply();
+
+    async function getInsult() {
+      try {
+        const requestInsult = await fetch(
+          "https://evilinsult.com/generate_insult.php?lang=en&type=json"
+        );
+        const responseToJson = await requestInsult.json();
+        return responseToJson;
+      } catch (err) {
+        throw err;
+      }
+    }
+
+    try {
+      const insult = await getInsult();
+
+      await interaction.followUp(`${user}, ${insult.insult}`);
+    } catch (error) {
+      await interaction.followUp(
+        `Couldn't retrieve Insult, I blame <@503264757785165851>`
+      );
+    }
+  },
+};
---
+++ commands/fun/jokes.js
@@ -0,0 +1,59 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { embedMessage } = require("../../modules/embedSimple");
+const fetch = require("node-fetch");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("joke")
+    .setDescription("gets a random joke!"),
+  async execute(interaction, client) {
+    await interaction.deferReply();
+    async function getJoke() {
+      try {
+        const requestInsult = await fetch(
+          "https://v2.jokeapi.dev/joke/Miscellaneous,Dark,Pun,Spooky,Christmas"
+        );
+        const responseToJson = await requestInsult.json();
+        return responseToJson;
+      } catch (err) {
+        throw err;
+      }
+    }
+
+    try {
+      const joke = await getJoke();
+
+      const embed = {
+        color: "#9dcc37",
+        title: `Collection of Jokes`,
+        description: `Dark Jokes ftw`,
+        fields: [
+          {
+            name: "Joke",
+            value: `${
+              joke.type === "twopart"
+                ? `${joke.setup}\n${joke.delivery}`
+                : `${joke.joke}`
+            }`,
+          },
+          {
+            name: "Category",
+            value: `${joke.category}`,
+            inline: true,
+          },
+          {
+            name: "Type",
+            value: `${joke.type}`,
+            inline: true,
+          },
+        ],
+      };
+
+      await interaction.followUp({ embeds: [embed] });
+    } catch (err) {
+      await interaction.followUp(
+        `Could not retrieve a Joke, I blame <@503264757785165851>`
+      );
+    }
+  },
+};
