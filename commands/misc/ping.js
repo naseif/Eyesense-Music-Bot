90b3ee3af8ce880c0adc9dc34443d8221792001b
naseif <d2mds21@gmail.com>
2021-08-26 12:52:44 +0200

    [New] Added ping command!

---
+++ commands/misc/ping.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("ping the bot connection to the server"),
+  async execute(interaction, client) {
+    await interaction.deferReply();
+
+    const embed = {
+      title: "Ping Pong!",
+      description: `📡 **Ping:** ${client.ws.ping}
+      ⏱ **Latency:** ${Date.now() - interaction.createdTimestamp}ms.`,
+      color: "#9dcc37",
+    };
+
+    await interaction.followUp({ embeds: [embed] });
+  },
+};
