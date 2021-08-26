dd6656ec64074ac0fa8f635814a7a3c979103a1c
naseif <d2mds21@gmail.com>
2021-08-26 10:31:11 +0200

    error handling for the stop command

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -6,9 +6,22 @@ module.exports = {
 
   async execute(interaction, client) {
     const queue = client.player.getQueue(interaction.guild);
-    if (queue) {
-      await interaction.reply(`Stopped ${queue.current.title}`);
+    await interaction.deferReply();
+
+    if (!queue || !queue.playing) {
+      return await interaction.followUp(
+        `<@${interaction.user.id}>, Nothing is playing to stop!`
+      );
+    }
+
+    const embed = {
+      color: "#9dcc37",
+      description: `✅ Stopped **${queue.current.title}** in [<#${interaction.member.voice.channelId}>]`,
+    };
+
+    if (queue && queue.playing) {
       await queue.stop();
+      await interaction.followUp({ embeds: [embed] });
     }
   },
 };
