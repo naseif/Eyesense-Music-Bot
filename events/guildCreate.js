501e6f0703ec5d26a6295c2af1f3ddc1285f42bc
naseif <d2mds21@gmail.com>
2021-08-27 11:09:19 +0000

    [New] Added guildCreate event which triggers commands registering when the bot joins a new server

---
+++ events/guildCreate.js
@@ -0,0 +1,8 @@
+const { registerSlashCommands } = require("../deploy-commands");
+
+module.exports = {
+  name: "guildCreate",
+  async execute(guild) {
+      await registerSlashCommands(guild.client.user.id, guild.id);
+  },
+};
