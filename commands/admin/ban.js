6aa00940c4f3bef4219829dba4affe46930e9d3f
naseif <d2mds21@gmail.com>
2021-08-30 10:12:45 +0000

    [Enhancement] Added embed message for the catch block and a condition to check if the user can ban members

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Permissions } = require("discord.js");
+const { embedMessage } = require("../../modules/embedSimple");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -17,6 +18,10 @@ module.exports = {
     await interaction.deferReply();
 
     const embed = {
+      author: {
+        name: `${interaction.user.username}`,
+        icon_url: `${interaction.user.avatarURL()}`,
+      },
       color: "#9dcc37",
       title: `A user has been Banned!`,
       fields: [
@@ -33,14 +38,26 @@ module.exports = {
       timestamp: new Date(),
     };
 
-    if (interaction.member.permissions.has([Permissions.FLAGS.BAN_MEMBERS])) {
-      try {
-        await interaction.guild.members.ban(user, { reason });
-        await interaction.followUp({ embeds: [embed] });
-      } catch (error) {
-        await interaction.followUp(`Couldn't ban ${user}, ${error.message}`);
-        console.log(error);
-      }
+    if (!interaction.member.permissions.has([Permissions.FLAGS.BAN_MEMBERS]))
+      return await interaction.followUp({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `❌ | You do not have permission to ban members!`
+          ),
+        ],
+      });
+
+    try {
+      await interaction.guild.members.ban(user, { reason });
+      await interaction.followUp({ embeds: [embed] });
+    } catch (error) {
+      await interaction.followUp({
+        embeds: [
+          embedMessage("#9dcc37", `Couldn't ban ${user}, ${error.message}`),
+        ],
+      });
+      console.log(error);
     }
   },
 };
