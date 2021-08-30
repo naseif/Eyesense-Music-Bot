468d54bb6303eec778e3cd0c2d164bae3ff53833
naseif <d2mds21@gmail.com>
2021-08-30 09:44:26 +0000

    [Fix] Added permission check for deleteMessages command. (Only members with MANAGE_MESSAGES permission flag)

--- commands/admin/deleteMessages.js
+++ commands/admin/deleteMessages.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { Permissions } = require("discord.js");
+const { embedMessage } = require("../../modules/embedSimple");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,6 +12,18 @@ module.exports = {
   async execute(interaction, client) {
     const integer = interaction.options.getInteger("int");
     await interaction.deferReply();
+
+    if (
+      !interaction.member.permissions.has([Permissions.FLAGS.MANAGE_MESSAGES])
+    )
+      return await interaction.followUp({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `❌ | You do not have permission to delete Messages!`
+          ),
+        ],
+      });
 
     try {
       const getMessages = await interaction.channel.messages.fetch({
