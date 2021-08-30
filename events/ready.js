e1c9be9b3247fea6e8e39c9427130737e0dc3ce8
naseif <68638710+naseif@users.noreply.github.com>
2021-08-30 14:19:43 +0200

    [New] Added activity

--- events/ready.js
+++ events/ready.js
@@ -14,5 +14,9 @@ module.exports = {
       }
     });
     console.log(`Ready! Logged in as ${client.user.tag}`);
+    const guilds = client.guilds.cache.map((guild) => guild.id);
+    client.user.setActivity(`${guilds.length} Servers!`, {
+      type: "WATCHING",
+    });
   },
 };
