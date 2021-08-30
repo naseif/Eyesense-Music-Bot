33cc673897d4d5bfe15c015b6e223c325bd60f49
naseif <d2mds21@gmail.com>
2021-08-30 08:42:31 +0000

    [New] Added a new perms command to check users roles and your own!

---
+++ commands/misc/perms.js
@@ -0,0 +1,59 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { embedMessage } = require("../../modules/embedSimple");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("perms")
+    .setDescription("Shows the permission for another member or your own")
+    .addUserOption((option) =>
+      option.setName("user").setDescription("Select a user")
+    ),
+  async execute(interaction, client) {
+    const user = interaction.options.getMember("user");
+    await interaction.deferReply();
+
+    let userRoles;
+
+    if (user) {
+      userRoles = await user.roles.cache.map((role) => {
+        if (role.name !== "@everyone") return `<@&${role.id}>`;
+      });
+    } else {
+      userRoles = await interaction.member.roles.cache.map((role) => {
+        if (role.name !== "@everyone") return `<@&${role.id}>`;
+      });
+    }
+
+    const permsEmbed = {
+      color: "#9dcc37",
+      title: `Roles list for ${
+        user ? user.user.username : interaction.user.username
+      }`,
+      author: {
+        name: `${interaction.user.username}`,
+        icon_url: `${interaction.user.avatarURL()}`,
+      },
+      description: `${userRoles.join("\n")}`,
+
+      timestamp: new Date(),
+      footer: {
+        text: "Created by naseif",
+        icon_url: "https://i.imgur.com/KrAvM8U.jpg",
+      },
+    };
+    try {
+      await interaction.followUp({
+        embeds: [permsEmbed],
+      });
+    } catch (err) {
+      await interaction.followUp({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `❌ | Ups! Looks like I do not have permission to list the roles or something else went wrong!`
+          ),
+        ],
+      });
+    }
+  },
+};
