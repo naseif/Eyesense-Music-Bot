8ce2a24c7aba95385b214018dd71e2ad8bf5a64a
naseif <d2mds21@gmail.com>
2021-08-30 16:52:24 +0000

    [Fix] Update Activity after joining and leaving servers

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,5 +4,9 @@ module.exports = {
   name: "guildCreate",
   async execute(guild) {
     await registerSlashCommands(guild.client.user.id, guild.id);
+    const guilds = guild.client.guilds.cache.map((guild) => guild.id);
+    guild.client.user.setActivity(`${guilds.length} Servers!`, {
+      type: "WATCHING",
+    });
   },
 };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,6 +1,10 @@
 module.exports = {
   name: "guildDelete",
   async execute(guild) {
+    const guilds = guild.client.guilds.cache.map((guild) => guild.id);
+    guild.client.user.setActivity(`${guilds.length} Servers!`, {
+      type: "WATCHING",
+    });
     try {
       await guild.commands.set([]);
     } catch (err) {
