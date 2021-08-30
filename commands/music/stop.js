90d82ac83cbaa87ab04aafae92c66aabd0a01831
naseif <d2mds21@gmail.com>
2021-08-30 15:28:42 +0000

    [New] Readded stop command based on a request

---
+++ commands/music/stop.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { embedMessage } = require("../../modules/embedSimple");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("stop")
+    .setDescription("stops the current song playing"),
+
+  async execute(interaction, client) {
+    const queue = client.player.getQueue(interaction.guild);
+    await interaction.deferReply();
+
+    if (!queue || !queue.playing) {
+      return await interaction.followUp({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `❌ | <@${interaction.user.id}>, Nothing is playing to stop!`
+          ),
+        ],
+      });
+    }
+
+    try {
+      if (queue && queue.playing) {
+        await interaction.followUp({
+          embeds: [
+            embedMessage(
+              "#9dcc37",
+              `✅ Stopped **${queue.current.title}** in [<#${interaction.member.voice.channelId}>]`
+            ),
+          ],
+        });
+        await queue.stop();
+      }
+    } catch (err) {
+      await interaction.followUp({
+        embeds: [embedMessage("#9dcc37", `❌ | Something went wrong :/`)],
+      });
+    }
+  },
+};
