c1bdff03fbbe17b6fac478264838c8390c03875d
naseif <d2mds21@gmail.com>
2021-09-02 06:33:16 +0000

    [New] Readded GUILD_MEMBERS intent and added GUILD_BANS intent

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const { Client, Collection, Intents } = require("discord.js");
 const { token } = require("./config.json");
 const client = new Client({
   intents: [
-    // Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MEMBERS,
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_BANS,
   ],
   restRequestTimeout: 30000,
 });
