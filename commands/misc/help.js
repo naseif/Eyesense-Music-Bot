7d695aaa7ae4a5940f4a6c5fcc54030c618ec8f4
naseif <d2mds21@gmail.com>
2021-09-02 05:07:51 +0000

    [New] help command will now show commands by category or all available commands!

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -3,21 +3,54 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("help")
-    .setDescription("Shows all available commands for this bot!"),
+    .setDescription("Shows all available commands for this bot!")
+    .addStringOption((option) =>
+      option
+        .setName("category")
+        .setDescription("show help by category")
+        .addChoice("Admin", "admin")
+        .addChoice("Fun", "fun")
+        .addChoice("Music", "music")
+        .addChoice("Misc", "misc")
+    ),
   async execute(interaction, client) {
     await interaction.deferReply();
-    const commands = client.commands.map(
-      (command) =>
-        "```" +
-        `/${command.data.name}` +
-        "```" +
-        "- " +
-        `**${command.data.description}**`
-    );
+    const category = interaction.options.getString("category");
+
+    function printHelpByCollection(collection) {
+      const commands = collection.map(
+        (command) =>
+          "```" +
+          `/${command.data.name}` +
+          "```" +
+          "- " +
+          `**${command.data.description}**`
+      );
+      return commands;
+    }
+
+    let help;
+    switch (category) {
+      case "admin":
+        help = printHelpByCollection(client.commands.admin);
+        break;
+      case "fun":
+        help = printHelpByCollection(client.commands.fun);
+        break;
+      case "music":
+        help = printHelpByCollection(client.commands.music);
+        break;
+      case "misc":
+        help = printHelpByCollection(client.commands.misc);
+        break;
+      default:
+        help = printHelpByCollection(client.commands);
+    }
+
     const embed = {
       color: "#9dcc37",
       title: `${client.user.username}'s Commands!`,
-      description: `${commands.join("\n")}`,
+      description: `${help.join("\n")}`,
       author: {
         name: `${interaction.user.username}`,
         icon_url: `${interaction.user.avatarURL()}`,
