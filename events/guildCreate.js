f446ea2443351751b48df3072a0ac247f1dcb49c
naseif <d2mds21@gmail.com>
2021-09-03 10:44:15 +0000

    [New] Added welcome message when the bot joins a new guild

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,6 +3,14 @@ const { registerSlashCommands } = require("../deploy-commands");
 module.exports = {
   name: "guildCreate",
   async execute(guild) {
+    if (guild.systemChannel) {
+      guild.systemChannel.send({
+        content: "**Thanks for adding me to your server!**",
+        files: ["https://nekos.best/api/v1/cuddle/017.gif"],
+      });
+    } else {
+      return;
+    }
     await registerSlashCommands(guild.client.user.id, guild.id);
     const guilds = guild.client.guilds.cache.map((guild) => guild.id);
     guild.client.user.setActivity(`${guilds.length} Servers!`, {
