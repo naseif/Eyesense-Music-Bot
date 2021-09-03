6e5fdeca9cac5651658df7e0acb0076a816824da
naseif <d2mds21@gmail.com>
2021-09-03 11:10:11 +0000

    [Fix] guild.commands.set() does not execute properly (client has no more access)

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -5,10 +5,5 @@ module.exports = {
     guild.client.user.setActivity(`${guilds.length} Servers!`, {
       type: "WATCHING",
     });
-    try {
-      await guild.commands.set([]);
-    } catch (err) {
-      console.error(`Something went wrong, ${err.message}`);
-    }
   },
 };
