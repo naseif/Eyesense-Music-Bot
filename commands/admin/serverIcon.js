887a0b5022fef1dfd624068459af52160fbe0c95
naseif <d2mds21@gmail.com>
2021-09-16 09:03:34 +0000

    [New] Added a command to change server icon!

---
+++ commands/admin/serverIcon.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { Permissions } = require("discord.js");
+const { embedMessage } = require("../../modules/embedSimple");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("servericon")
+    .setDescription("chnages the server icon!")
+    .addStringOption((option) =>
+      option
+        .setName("link")
+        .setDescription("the link of the new img, must end with png or jpg!")
+        .setRequired(true)
+    ),
+  async execute(interaction, client) {
+    await interaction.deferReply();
+    const newIcon = interaction.options.getString("link");
+
+    if (
+      !interaction.member.permissions.has([Permissions.FLAGS.MANAGE_GUILD]) ||
+      !interaction.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR])
+    )
+      return await interaction.followUp({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `${interaction.user.toString()}, You do not have permission to change the server icon!`
+          ),
+        ],
+      });
+
+    try {
+      await interaction.guild.setIcon(newIcon);
+      await interaction.followUp({
+        embeds: [
+          embedMessage("#9dcc37", `Server Icon has been successfully changed!`),
+        ],
+      });
+    } catch (error) {
+      await interaction.followUp({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `Could not change the server icon!, ${error.message}`
+          ),
+        ],
+      });
+    }
+  },
+};
