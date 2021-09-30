a3d0d329fbb9ddefa145e2fadc77ae08b7a3d51c
naseif <d2mds21@gmail.com>
2021-09-30 10:01:08 +0000

    [Update] updated mstoTime function

--- modules/mstoTime.js
+++ modules/mstoTime.js
@@ -1,12 +1,16 @@
 module.exports.msToTime = (duration) => {
   let milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    seconds = parseInt((duration / 1000) % 60),
+    minutes = parseInt((duration / (1000 * 60)) % 60),
+    hours = parseInt((duration / (1000 * 60 * 60)) % 24);
 
   hours = hours < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+  if (duration < 3600000) {
+    return minutes + ":" + seconds;
+  } else {
+    return hours + ":" + minutes + ":" + seconds;
+  }
 };
