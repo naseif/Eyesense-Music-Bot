8465c967392295698dfc6e8cf97482db1db4b4c5
naseif <d2mds21@gmail.com>
2021-10-01 08:46:19 +0000

    [New] Added 8D audio filter for music

---
+++ commands/music/8D.js
@@ -0,0 +1,62 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { embedMessage } = require("../../modules/embedSimple");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("8d")
+    .setDescription("8D Audio Filter")
+    .addStringOption((option) =>
+      option
+        .setName("mode")
+        .setDescription("ON/OFF")
+        .setRequired(true)
+        .addChoice("ON", "1")
+        .addChoice("OFF", "0")
+    ),
+
+  async execute(interaction, client) {
+    await interaction.deferReply();
+    const queue = client.player.getQueue(interaction.guild);
+    const mode = interaction.options.getString("mode");
+
+    if (!queue)
+      return await interaction.followUp({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `Your Queue is empty, Make sure to play a song first`
+          ),
+        ],
+      });
+
+    switch (mode) {
+      case "1":
+        try {
+          await queue.setFilters({
+            "8D": true,
+          });
+          await interaction.followUp({
+            embeds: [embedMessage("#9dcc37", `✅ 8D Filter has been enabled`)],
+          });
+        } catch (error) {
+          client.logger(error.message, "error");
+          await interaction.followUp({
+            embeds: [embedMessage("#9dcc37", `Could not set the Filter`)],
+          });
+        }
+        break;
+      case "0":
+        try {
+          await queue.setFilters({ "8D": false });
+          await interaction.followUp({
+            embeds: [embedMessage("#9dcc37", `✅ 8D Filter has been disabled`)],
+          });
+        } catch (error) {
+          client.logger(error.message, "error");
+          await interaction.followUp({
+            embeds: [embedMessage("#9dcc37", `Could not disable the filter`)],
+          });
+        }
+    }
+  },
+};
