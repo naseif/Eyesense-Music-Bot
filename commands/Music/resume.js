63e36a2ed5a631236a8311e3937af0eeb42c3c87
naseif <d2mds21@gmail.com>
2021-10-04 12:17:17 +0000

    [New] More updates to the music commands

--- commands/Music/resume.js
+++ commands/Music/resume.js
@@ -2,9 +2,45 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { embedMessage } = require("../../modules/embedSimple");
 
 module.exports = {
+  name: "resume",
+  args: false,
+  description: "Resumes a paused song",
+  aliases: ["r"],
+  usage: "r || resume",
+  async run(message, args, client) {
+    const queue = client.player.getQueue(message.guild);
+
+    if (!queue || !queue.playing)
+      return await message.channel.send({
+        embeds: [
+          embedMessage("#9dcc37", `❌ | There is nothing playing to resume!`),
+        ],
+      });
+
+    try {
+      if (queue) {
+        await queue.setPaused(false);
+        await message.channel.send({
+          embeds: [
+            embedMessage(
+              "#9dcc37",
+              `✅ **${
+                queue.current.title
+              }** resumed [${message.member.toString()}]`
+            ),
+          ],
+        });
+      }
+    } catch (err) {
+      client.logger(err.message, "error");
+      await message.channel.send({
+        embeds: [embedMessage("#9dcc37", "I was not able to resume this song")],
+      });
+    }
+  },
   data: new SlashCommandBuilder()
     .setName("resume")
-    .setDescription("resumes the song"),
+    .setDescription("Resumes a paused song"),
 
   async execute(interaction, client) {
     await interaction.deferReply();
@@ -24,7 +60,9 @@ module.exports = {
           embeds: [
             embedMessage(
               "#9dcc37",
-              `✅ **${queue.current.title}** resumed [<@${interaction.user.id}>]`
+              `✅ **${
+                queue.current.title
+              }** resumed [${interaction.member.toString()}]`
             ),
           ],
         });
--- commands/Music/shuffle.js
+++ commands/Music/shuffle.js
@@ -2,9 +2,40 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { embedMessage } = require("../../modules/embedSimple");
 
 module.exports = {
+  name: "shuffle",
+  args: false,
+  description: "Shuffles The Music Queue",
+  usage: "shuffle",
+  async run(message, args, client) {
+    const queue = client.player.getQueue(message.guild);
+
+    if (!queue)
+      return await message.channel.send({
+        embeds: [embedMessage("#9dcc37", `❌ | There is no queue to shuffle!`)],
+      });
+
+    try {
+      if (queue) {
+        await queue.shuffle();
+        await message.channel.send({
+          embeds: [
+            embedMessage(
+              "#9dcc37",
+              `✅ Queue has been shuffled [${message.member.toString()}]`
+            ),
+          ],
+        });
+      }
+    } catch (error) {
+      client.logger(error.message, "error");
+      await message.channel.send({
+        embeds: [embedMessage("#9dcc37", "Could not shuffle the queue")],
+      });
+    }
+  },
   data: new SlashCommandBuilder()
     .setName("shuffle")
-    .setDescription("shuffles the music queue"),
+    .setDescription("Shuffles The Music Queue"),
 
   async execute(interaction, client) {
     await interaction.deferReply();
@@ -22,7 +53,7 @@ module.exports = {
           embeds: [
             embedMessage(
               "#9dcc37",
-              `✅ Queue has been shuffled [<@${interaction.user.id}>]`
+              `✅ Queue has been shuffled [${interaction.member.toString()}]`
             ),
           ],
         });
