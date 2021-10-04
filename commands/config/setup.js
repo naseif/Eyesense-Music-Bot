c83e6e60a3a3c82a91a6bf1d4e7762325c8d7f56
naseif <d2mds21@gmail.com>
2021-10-04 08:48:00 +0000

    [New] Created setup command to set custom prefix for servers in the database

---
+++ commands/config/setup.js
@@ -0,0 +1,127 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { embedMessage } = require("../../modules/embedSimple");
+const { prefix } = require("../../config.json");
+
+module.exports = {
+  name: "prefix",
+  aliases: ["setup"],
+  description: "set custom prefix for your server",
+  args: true,
+  usage: `${prefix}prefix set <new prefix>`,
+  async run(message, args, client, defaultPrefix) {
+    if (!message.member.permissions.has("MANAGE_GUILD"))
+      return await message.channel.send({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `❌ You do not have permission to change the prefix!`
+          ),
+        ],
+      });
+
+    if (!args[0])
+      return await message.channel.send({
+        embeds: [
+          embedMessage("#9dcc37", `❌ You did not provide your new prefix!`),
+        ],
+      });
+
+    if (args[1])
+      return await message.channel.send({
+        embeds: [embedMessage("#9dcc37", `❌ Prefix can't have 2 arguments`)],
+      });
+
+    if (args.join(" ") === prefix) {
+      await client.db.delete(`guild_prefix_${message.guildId}`);
+      return await message.channel.send({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `✅ Bot Prefix Resetted to ` + "`" + `${prefix}` + "`"
+          ),
+        ],
+      });
+    }
+
+    try {
+      await client.db.set(`guild_prefix_${message.guildId}`, args[0]);
+      await message.channel.send({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `✅ Bot Prefix set to ` + "`" + `${args[0]}` + "`"
+          ),
+        ],
+      });
+    } catch (err) {
+      client.logger(err.message, "error");
+      await message.channel.send({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `❌ There has been an error, Could not set the new Prefix`
+          ),
+        ],
+      });
+    }
+  },
+
+  data: new SlashCommandBuilder()
+    .setName("prefix")
+    .setDescription("set custom prefix for your server")
+    .addStringOption((option) =>
+      option
+        .setName("newprefix")
+        .setDescription("Your new Prefix")
+        .setRequired(true)
+    ),
+  async execute(interaction, client) {
+    const newPrefix = interaction.options.getString("newprefix");
+    await interaction.deferReply();
+
+    if (!interaction.member.permissions.has("MANAGE_GUILD")) {
+      return await interaction.followUp({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `You do not have permission to change the prefix`
+          ),
+        ],
+      });
+    }
+
+    if (newPrefix === prefix) {
+      await client.db.delete(`guild_prefix_${interaction.guildId}`);
+      return await interaction.followUp({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `✅ Bot Prefix Resetted to ` + "`" + `${prefix}` + "`"
+          ),
+        ],
+      });
+    }
+
+    try {
+      await client.db.set(`guild_prefix_${interaction.guildId}`, newPrefix);
+      await interaction.followUp({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `✅ Bot Prefix set to ` + "`" + `${newPrefix}` + "`"
+          ),
+        ],
+      });
+    } catch (err) {
+      client.logger(err.message, "error");
+      await interaction.followUp({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `❌ There has been an error, Could not set the new Prefix`
+          ),
+        ],
+      });
+    }
+  },
+};
