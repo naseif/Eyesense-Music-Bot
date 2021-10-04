f8be8e866dc0a2862650b40548d5a884a1f64ddd
naseif <d2mds21@gmail.com>
2021-10-04 12:09:31 +0000

    [New] skip and stop work now with prefixes

--- commands/Music/skip.js
+++ commands/Music/skip.js
@@ -2,13 +2,48 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { embedMessage } = require("../../modules/embedSimple");
 
 module.exports = {
+  name: "skip",
+  args: false,
+  description: "Skips to the next song in the Queue",
+  aliases: ["s"],
+  usage: "s || skip",
+  async run(message, args, client) {
+    const queue = client.player.getQueue(message.guild);
+
+    if (!queue || !queue.playing)
+      return await message.channel.send({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `❌ | No music is being played! [${message.member.toString()}]`
+          ),
+        ],
+      });
+
+    try {
+      const currnetSong = queue.current;
+      await queue.skip();
+      return await message.channel.send({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `Skipped **${currnetSong.title}**, [${message.member.toString()}]`
+          ),
+        ],
+      });
+    } catch (err) {
+      client.logger(err.message, "error");
+      await message.channel.send({
+        embeds: [embedMessage("#9dcc37", "Song could not be skipped")],
+      });
+    }
+  },
   data: new SlashCommandBuilder()
     .setName("skip")
     .setDescription("skips a song from the queue"),
 
   async execute(interaction, client) {
     await interaction.deferReply();
-    const usermention = `<@${interaction.member.id}>`;
     const queue = client.player.getQueue(interaction.guild);
 
     if (!queue || !queue.playing)
@@ -16,7 +51,7 @@ module.exports = {
         embeds: [
           embedMessage(
             "#9dcc37",
-            `❌ | No music is being played! [${usermention}]`
+            `❌ | No music is being played! [${interaction.member.toString()}]`
           ),
         ],
       });
@@ -28,7 +63,9 @@ module.exports = {
         embeds: [
           embedMessage(
             "#9dcc37",
-            `Skipped **${currnetSong.title}**, [<@${interaction.user.id}>]`
+            `Skipped **${
+              currnetSong.title
+            }**, [${interaction.member.toString()}]`
           ),
         ],
       });
--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -2,6 +2,43 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { embedMessage } = require("../../modules/embedSimple");
 
 module.exports = {
+  name: "stop",
+  args: false,
+  description: "Stops the player and leaves the voice channel",
+  usage: "stop",
+  async run(message, args, client) {
+    const queue = client.player.getQueue(message.guild);
+
+    if (!queue || !queue.playing) {
+      return await message.channel.send({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `❌ | ${message.member.toString()}, Nothing is playing to stop!`
+          ),
+        ],
+      });
+    }
+
+    try {
+      if (queue && queue.playing) {
+        await message.channel.send({
+          embeds: [
+            embedMessage(
+              "#9dcc37",
+              `✅ Stopped **${queue.current.title}** in [<#${message.member.voice.channel}>]`
+            ),
+          ],
+        });
+        await queue.stop();
+      }
+    } catch (err) {
+      client.logger(err.message, "error");
+      await message.channel.send({
+        embeds: [embedMessage("#9dcc37", `❌ | Something went wrong :/`)],
+      });
+    }
+  },
   data: new SlashCommandBuilder()
     .setName("stop")
     .setDescription("stops the current song playing"),
@@ -15,7 +52,7 @@ module.exports = {
         embeds: [
           embedMessage(
             "#9dcc37",
-            `❌ | <@${interaction.user.id}>, Nothing is playing to stop!`
+            `❌ | ${interaction.member.toString()}, Nothing is playing to stop!`
           ),
         ],
       });
