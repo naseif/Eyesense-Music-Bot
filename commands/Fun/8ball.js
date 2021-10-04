25d5942af5c9f4fd1eeecb27a5d2ce5f2dd5254d
naseif <d2mds21@gmail.com>
2021-10-04 15:59:28 +0000

    [New] Added prefix support for 8ball and jokes commands

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -1,6 +1,44 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { embedMessage } = require("../../modules/embedSimple");
 
 module.exports = {
+  name: "8ball",
+  args: true,
+  usage: "8ball <question>",
+  description: "Ask the magic ball a question!",
+  async run(message) {
+    const question = args.join(" ");
+
+    if (!question)
+      return await message.channel.send({
+        embeds: [embedMessage("9dcc37", `You did not ask me anything!`)],
+      });
+
+    const answers = [
+      "Aboslutely 👌",
+      "I am not sure, but maybe? 🤐",
+      "Yes! 💯",
+      "No! 😡",
+      "Uhh, yeah sure 😕",
+      "Mhmm, why not 🤷‍♂️",
+      "Hell no! 🤫",
+      "I won't answer that 🤥",
+      "That's a bit lewd 🤤",
+    ];
+
+    const embed = {
+      color: "#9dcc37",
+      title: `${question}`,
+      author: {
+        name: `${message.member.user.username}`,
+        icon_url: `${message.member.user.avatarURL()}`,
+      },
+      description: `${answers[Math.floor(Math.random() * answers.length)]}`,
+    };
+    await message.channel.send({
+      embeds: [embed],
+    });
+  },
   data: new SlashCommandBuilder()
     .setName("8ball")
     .setDescription("ask the magic ball a question!")
--- commands/Fun/jokes.js
+++ commands/Fun/jokes.js
@@ -2,6 +2,50 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { requestAPI } = require("../../modules/requestAPI");
 
 module.exports = {
+  name: "joke",
+  args: false,
+  usage: "joke <question>",
+  description: "Gets a random joke!",
+  async run(message, args, client) {
+    try {
+      const joke = await requestAPI(
+        "https://v2.jokeapi.dev/joke/Miscellaneous,Dark,Pun,Spooky,Christmas"
+      );
+
+      const embed = {
+        color: "#9dcc37",
+        title: `Collection of Jokes`,
+        description: `Dark Jokes ftw`,
+        fields: [
+          {
+            name: "Joke",
+            value: `${
+              joke.type === "twopart"
+                ? `${joke.setup}\n${joke.delivery}`
+                : `${joke.joke}`
+            }`,
+          },
+          {
+            name: "Category",
+            value: `${joke.category}`,
+            inline: true,
+          },
+          {
+            name: "Type",
+            value: `${joke.type}`,
+            inline: true,
+          },
+        ],
+      };
+
+      await message.channel.send({ embeds: [embed] });
+    } catch (err) {
+      client.logger(err.message, "error");
+      await message.channel.send(
+        `Could not retrieve a Joke, I blame <@503264757785165851>`
+      );
+    }
+  },
   data: new SlashCommandBuilder()
     .setName("joke")
     .setDescription("gets a random joke!"),
