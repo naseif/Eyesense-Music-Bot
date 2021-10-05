85d8d86971842a8d0533886c7e7984af18c99cac
naseif <d2mds21@gmail.com>
2021-10-05 08:36:24 +0000

    [New] Added getGuildUser that parses the mention and returns the GuildUser class

--- modules/getUserFromMention.js
+++ modules/getUserFromMention.js
@@ -18,3 +18,23 @@ module.exports.getUserFromMention = (mention, client) => {
     return client.users.cache.get(mention);
   }
 };
+
+/**
+ * ´Gets the GuildMember Object
+ * @param {mention} mention
+ * @param {message} message
+ * @returns GuildMember Object
+ */
+module.exports.getGuildUserFromMention = (mention, message) => {
+  if (!mention) return;
+
+  if (mention.startsWith("<@") && mention.endsWith(">")) {
+    mention = mention.slice(2, -1);
+
+    if (mention.startsWith("!")) {
+      mention = mention.slice(1);
+    }
+
+    return message.guild.members.cache.get(mention);
+  }
+};
