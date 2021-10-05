a69b7589a592b814b21f4680d3138cc48356f853
naseif <d2mds21@gmail.com>
2021-10-05 07:33:55 +0000

    [New] invite command now supports prefixes

--- commands/Misc/invite.js
+++ commands/Misc/invite.js
@@ -2,6 +2,44 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js");
 
 module.exports = {
+  name: "invite",
+  aliases: ["inv"],
+  description: "Get my link to invite me to other servers!",
+  usage: "invite || inv",
+  async run(message, args, client) {
+    const row = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setLabel("Invite")
+        .setStyle("LINK")
+        .setURL(
+          `https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=applications.commands%20bot&permissions=536079414`
+        ),
+      new MessageButton()
+        .setLabel("GitHub")
+        .setStyle("LINK")
+        .setURL("https://github.com/naseif/Eyesense-Music-Bot"),
+      new MessageButton()
+        .setLabel("Support")
+        .setStyle("LINK")
+        .setURL("https://discord.gg/JCdpeeNP9N")
+    );
+
+    const embed = new MessageEmbed()
+      .setAuthor(
+        "Eyesense",
+        "https://cdn.discordapp.com/attachments/547844388492148737/893093188926079036/c8e3bbbc07dc4680bea5b355f2c708c7.png"
+      )
+      .setThumbnail(
+        "https://cdn.discordapp.com/attachments/547844388492148737/893093188926079036/c8e3bbbc07dc4680bea5b355f2c708c7.png"
+      )
+      .setColor("#9dcc37")
+      .addField(
+        "Eyesense",
+        `Eyesense is a feature-rich discord bot that provides over 100 commands!`,
+        true
+      );
+    await message.channel.send({ embeds: [embed], components: [row] });
+  },
   data: new SlashCommandBuilder()
     .setName("invite")
     .setDescription("Get my link to invite me to other servers!"),
