ddda85d7fd0d7aced2906337f1fabfe490913a45
naseif <d2mds21@gmail.com>
2021-11-02 09:25:27 +0000

    [New] DJ Command implemented, Users should now be able to set a role to control the play command

--- commands/Config/dj.js
+++ commands/Config/dj.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { getRoleFromMention } = require("../../modules/getUserFromMention");
+const { embedMessage } = require("../../modules/embedSimple");
 
 module.exports = {
   name: "dj",
@@ -7,6 +8,16 @@ module.exports = {
   args: true,
   usage: `dj set <role>`,
   async run(message, args, client, defaultPrefix) {
+    if (!message.member.permissions.has("MANAGE_GUILD"))
+      return await message.channel.send({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `❌ You do not have permission to grant the DJ Role!`
+          ),
+        ],
+      });
+
     if (!args[0]) {
       return await message.channel.send({
         embeds: [
@@ -19,7 +30,54 @@ module.exports = {
     }
 
     if (args[0] === "set" && args[1]) {
-      const test = await getRoleFromMention(args[1], message);
+      const newDjRole = await getRoleFromMention(args[1], message);
+      if (!newDjRole)
+        return await message.channel.send({
+          embeds: [
+            embedMessage("#9dcc37", `The Role you mentioned is not valid!`),
+          ],
+        });
+
+      await client.db.set(`djRole_${message.guildId}`, newDjRole.id);
+      await message.channel.send({
+        embeds: [
+          embedMessage(
+            "#9dcc37",
+            `✅ DJ Role has been set <@&${newDjRole.id}>`
+          ),
+        ],
+      });
+    }
+    if (args[0] === "delete" && args[1]) {
+      const roleToDelete = await getRoleFromMention(args[1], message);
+      const roleExist = await client.db.get(`djRole_${message.guildId}`);
+      if (!roleToDelete)
+        return await message.channel.send({
+          embeds: [
+            embedMessage("#9dcc37", `The Role you mentioned is not valid!`),
+          ],
+        });
+      if (roleToDelete.id === roleExist) {
+        await client.db.delete(`djRole_${message.guildId}`, roleToDelete.id);
+        return await message.channel.send({
+          embeds: [
+            embedMessage(
+              "#9dcc37",
+              `✅ DJ Role has been deleted <@&${roleToDelete.id}>`
+            ),
+          ],
+        });
+      }
+      if (roleToDelete.id !== roleExist) {
+        return await message.channel.send({
+          embeds: [
+            embedMessage(
+              "#9dcc37",
+              `The Role you wish to delete is not registered as DJ Role\nRegistered Role: <@&${roleExist}>`
+            ),
+          ],
+        });
+      }
     }
   },
   data: new SlashCommandBuilder()
