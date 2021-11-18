0f1cb080bd6fb7c66575c97ad9bd819d3cecddfe
naseif <d2mds21@gmail.com>
2021-11-18 09:37:14 +0000

    [New] Added `guildMemberRemove` event

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,4 +1,54 @@
 module.exports = {
   name: "guildMemberRemove",
-  async execute(guildmember) {},
+  async execute(guild) {
+    const checkCustomChannel = await guild.client.db.get(
+      `leave_${guild.guild.id}`
+    );
+
+    const welcomeEmbed = {
+      color: "#9dcc37",
+      title: `Member left the server!`,
+      thumbnail: {
+        url: `${
+          guild.user.avatarURL() ||
+          guild.guild.iconURL() ||
+          guild.guild.client.user.avatarURL()
+        }`,
+      },
+      fields: [
+        {
+          name: ":man_detective: Member",
+          value: `${guild.toString()}`,
+        },
+        {
+          name: ":clock2: Joined Server",
+          value: `${guild.joinedAt.toDateString()}`,
+        },
+        {
+          name: ":calendar_spiral: Joined Discord",
+          value: `${guild.user.createdAt.toDateString()}`,
+        },
+      ],
+    };
+
+    const sendWelcome = (channel) => {
+      return channel.send({
+        embeds: [welcomeEmbed],
+      });
+    };
+
+    if (checkCustomChannel) {
+      const customchannel = guild.guild.channels.cache.find(
+        (channel) => channel.id === checkCustomChannel
+      );
+      if (!customchannel) return;
+      return sendWelcome(customchannel);
+    }
+
+    if (guild.guild.systemChannel) {
+      return sendWelcome(guild.guild.systemChannel);
+    } else {
+      return;
+    }
+  },
 };
